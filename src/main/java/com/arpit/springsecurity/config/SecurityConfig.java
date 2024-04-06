cb2e2241f6fe78b67a81fc407a54d369f3dcd7a6
arpit05va <arpitvashistha928@gmail.com>
2024-04-06 17:00:06 +0530

    implement inMemoryDatabaseAuthentication

--- src/main/java/com/arpit/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/arpit/springsecurity/config/SecurityConfig.java
@@ -3,21 +3,50 @@ package com.arpit.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails normalUser = User
+                                    .withUsername("normal")
+                                    .password(passwordEncoder().encode("password"))
+                                    .roles("NORMAL")
+                                    .build();
+        UserDetails adminUser = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+
+    @Bean
     public SecurityFilterChain  filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().authorizeHttpRequests()
-                .requestMatchers("/public")
-                .permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/user").hasAnyRole("NORMAL", "ADMIN")
+                .requestMatchers("/public").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .formLogin();
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
